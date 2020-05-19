b49e3ec7e4ade45fc2c65293f13652ea6e374e29
Daniel Ewers <daniel-ewers2@live.nmit.ac.nz>
2020-05-19 19:05:57 +1200

    Rec change

--- recursive.js
+++ recursive.js
@@ -1,7 +1,5 @@
 function recursiveFunc(myNumber) { //Creates our recursive function called "recursiveFunc" with the parameter myNumber
-   
-console.log(myNumber);//prints myNumber "10, 9, 8, 7, 6, 5, 4, 3, 2, 1"
-   
+    
 let nextVal = myNumber - 1; //The next value will be the previous value -1
     /*
     i.e
@@ -13,7 +11,10 @@ let nextVal = myNumber - 1; //The next value will be the previous value -1
     if (nextVal > 0) { //If the next value will be greater than 0, call the function to the value -1
         recursiveFunc(nextVal);
     }
+    console.log(myNumber);//prints myNumber "10, 9, 8, 7, 6, 5, 4, 3, 2, 1"
 }
+
+
 recursiveFunc(10); 
 //Calling the function with the paramater myNumber assigned as 10
 //This means that when we console log (10), it will count down from 10
